eff50cdba473e92018b77b628513d20b54155ca9
PIG208 <359101898@qq.com>
2021-10-06 08:39:18 -0400

    Create home btn with js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
-document.getElementsByClassName("btn-home")[0].addEventListener("click", (e)=>{
-    window.location.href = "/";
+let btnHome = document.createElement("button");
+btnHome.innerHTML = "Home";
+btnHome.classList.add("btn-home");
+document.getElementsByTagName("main")[0].appendChild(btnHome);
+btnHome.addEventListener("click", (e)=>{
+    if(window.location.hostname === "pig208.github.io"){
+        window.location.href = "/Webdev";
+    }
+    else{
+        window.location.href = "/";
+    }
 })
